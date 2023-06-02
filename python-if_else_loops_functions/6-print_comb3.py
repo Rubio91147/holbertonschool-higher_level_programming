83c582e2614276dba282236e360a69558918c0a5
rubio91147 <rubendariofb@gmail.com>
2023-06-02 12:18:39 -0500

    Inventing is a combination of brains and materials.

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for item in range(10):
+    for item2 in range(item + 1, 10):
+        if item == 8 and item2 == 9:
+            print("{}{}".format(item, item2), end="")
+        else:
+            print("{}{}".format(item, item2), end="")
