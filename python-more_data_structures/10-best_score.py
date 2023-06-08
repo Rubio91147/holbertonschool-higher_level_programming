0ec7d66da3f0cf336128d11b077a5fd6b3fb0b66
rubio91147 <rubendariofb@gmail.com>
2023-06-08 14:05:09 -0500

    10. Best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
-def best_score(my_dict):
-    if my_dict:
-        return (max(my_dict))
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    values = []
+    for key in a_dictionary:
+        values.append(a_dictionary[key])
+
+    max_value = max(values)
+
+    best_keys = []
+    for key in a_dictionary:
+        if a_dictionary[key] == max_value:
+            best_keys.append(key)
+
+    return best_keys[0]
