f3af0a302934167f5cee9fb3fc1976292eb4b1b4
rubio91147 <rubendariofb@gmail.com>
2023-06-16 15:06:50 -0500

    0. Simple rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""Write an empty class Rectangle that defines a rectangle:"""
+
+
 class Rectangle:
+    """
+    Represents a rectangle with a given length and width.
+    """
     pass
