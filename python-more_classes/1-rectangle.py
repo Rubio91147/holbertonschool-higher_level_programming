7dae5d58e1a664cedbab9f18722651e1d2486a4c
rubio91147 <rubendariofb@gmail.com>
2023-06-16 15:33:02 -0500

    update

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-'''
-    Defines a Rectangle
-'''
+
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
+    """Represent a rectangle."""
+
     def __init__(self, width=0, height=0):
-        '''
-            Calling the setters to initialize the instance variables
-        '''
-        self.height = height
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.width = width
+        self.height = height
 
     @property
     def width(self):
-        '''
-            Returning the private var
-        '''
+        """Get/set the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        '''
-            Checking for TypeError and ValueError
-            then setting up the private var
-        '''
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -33,17 +31,11 @@ class Rectangle:
 
     @property
     def height(self):
-        '''
-            Returning the private var
-        '''
+        """Get/set the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        '''
-            Checking for TypeError and ValueError
-            then setting up the private var
-        '''
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
