3b01680b4e8a43ec844f0077f37f69d8f687f9ac
Rubio91147 <124604809+Rubio91147@users.noreply.github.com>
2023-07-25 12:13:54 -0500

    Update 4-first_table.sql

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
---Write a script that creates a table called first_table in the current database in your MySQL server.
-CREATE_TABLE_SQL="IF NOT EXISTS first_TABLE (
-    id INT,
-    name VARCHAR(256)
+-- Write a script that creates a table called first_table in the current database in your MySQL server.
+CREATE TABLE IF NOT EXISTS first_table (
+       id INT,
+       name VARCHAR(256)
 );
